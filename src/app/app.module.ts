ab404b28bfcb70d0733db4872662a6a945c7d310
Nazarov Oleksiy <sonynazarov@gmail.com>
2023-05-08 11:49:40 +0300

    {

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoaderComponent } from './loader/loader.component';
-import {AuthInterceptorService} from "./auth-interceptor.service";
+import { AuthInterceptorService } from "./auth-interceptor.service";
+import { LoggingInterceptorService } from "./logging-interceptor.service";
 
 @NgModule({
   declarations: [AppComponent, LoaderComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [
     {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true }
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptorService,
+      multi: true
+    },
   ],
   bootstrap: [AppComponent]
 })
